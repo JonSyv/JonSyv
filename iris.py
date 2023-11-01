374f99b60eca757d8c0198beae75513de12e340a
JonSyv <101867839+JonSyv@users.noreply.github.com>
2023-11-01 20:19:15 +0100

    Add files via upload

---
+++ iris.py
@@ -0,0 +1,18 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+iris = datasets.load_iris()
+
+features = iris.data
+labels = iris.target
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=.5)
+
+my_classifier = KNeighborsClassifier()
+my_classifier.fit(features_train, labels_train)
+
+prediction = my_classifier.predict(features_test)
+
+print(accuracy_score(labels_test, prediction))
---
+++ time.py
@@ -0,0 +1,6 @@
+import datetime
+now = datetime.datetime.now()
+print("Tiden er nå : ")
+print(now.strftime("%Y-%m-%d %H:%M-%S"))
+
+"""viser tiden pr.nå"""
